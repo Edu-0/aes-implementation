62eaca7793569e856910c7c79eb1a43c8d5adfb7
Edu-0 <eduardojose.de.souza332@gmail.com>
2026-03-13 16:14:19 -0300

    feat: added the decrypt text function and finished the basic operations done by AES (encrypt and encrypt) and an extra generate key func

--- src/controller/control.py
+++ src/controller/control.py
@@ -3,8 +3,9 @@ from pathlib import Path
 import src.model.cipher as cipher
 import src.utils.text_converter as tc
 
+
 def get_files_dir():
-    src_dir = Path("ui.py").resolve().parents[2]
+    src_dir = Path("ui.py").resolve().parents[0]
     files_dir = f"{src_dir}\\files"
 
     if not os.path.exists(files_dir):
@@ -29,7 +30,8 @@ def encrypt_plaintext(files_dir):
                 print("Invalid plaintext for encryption")
     except FileNotFoundError:
         print("\"plaintext.txt\" file not found in \"files\" directory")
-    print(plaintext)
+        return
+
     try:
         key_path = os.path.join(files_dir, "key.txt")
         with open(key_path, "r") as file:
@@ -38,10 +40,9 @@ def encrypt_plaintext(files_dir):
                 print("Invalid key read from files, it must have a size of 32 chars in hex")
     except FileNotFoundError:
         print(f"Key file not found, add a key.txt file in \"files\" folder")
+        return
 
-    print(initial_key)
-
-    key_array = tc.string_to_array(initial_key)
+    key_array = tc.key_string_to_array(initial_key)
 
     byte_blocks = tc.start_encoding_conversion(plaintext)
     rk_list = cipher.expand_key(key_array)
@@ -53,3 +54,42 @@ def encrypt_plaintext(files_dir):
     with open(encrypted_path, "w") as file:
         file.write(tc.byte_blocks_to_hex_string(final_encrypted))
     print("Text encrypted and saved on files folder")
+
+
+def decrypt_text(files_dir):
+    try:
+        encrypted_path = os.path.join(files_dir, "encrypted.txt")
+        with open(encrypted_path, "r", encoding='utf-8') as file:
+            encrypted = file.read()
+            if len(encrypted) < 1:
+                print("Invalid encrypted for decryption")
+    except FileNotFoundError:
+        print("\"encrypted.txt\" file not found in \"files\" directory")
+        return
+
+    try:
+        key_path = os.path.join(files_dir, "key.txt")
+        with open(key_path, "r") as file:
+            initial_key = file.readline()
+            if len(initial_key) < 32:
+                print("Invalid key read from files, it must have a size of 32 chars in hex")
+    except FileNotFoundError:
+        print(f"Key file not found, add a key.txt file in \"files\" folder")
+        return
+
+    key_array = tc.key_string_to_array(initial_key)
+
+    byte_list = tc.hex_string_to_byte_list(encrypted)
+    encrypted_text_array = tc.array_creator(byte_list)
+
+    rk_list = cipher.expand_key(key_array)
+    decrypted_block = cipher.decrypt(encrypted_text_array, rk_list)
+
+    decrypted_path = os.path.join(files_dir, "decrypted.txt")
+    with open(decrypted_path, "w", encoding='utf-8') as file:
+        try:
+            file.write(tc.start_decoding_conversion(decrypted_block))
+        except UnicodeDecodeError:
+            print("Erro: chave de descriptografia incorreta ou arquivo corrompido.")
+            return
+    print("Text decrypted and saved on files folder")
