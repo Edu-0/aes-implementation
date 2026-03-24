e3223f025881eed8be0213e47319be7dcacbe2d0
Edu-0 <eduardojose.de.souza332@gmail.com>
2026-03-24 17:18:10 -0300

    docs: warnings about vulnerabilities

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from src.view.ui import start
 
+# CTR Mode implemented
+# For academic and learning purposes only. Not secure for production
+# Vulnerabilities include: Side-channel leaks (timing, cache, power, EM), nonce reuse, lack of authentication, etc...
+
 if __name__ == "__main__":
     start()
